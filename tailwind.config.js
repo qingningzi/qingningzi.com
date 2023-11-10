d1088be700a82944fd6f129a6be943e98497e3a4
cleveng <cleveng@gmail.com>
2023-11-10 18:03:58 +0800

    add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('daisyui'),
+    ],
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 const {resolve} = require('path')
 import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import tailwindcss from 'tailwindcss'
 
 export default ({mode}) => {
     process.env = {...process.env, ...loadEnv(mode, process.cwd())}
@@ -13,6 +14,15 @@ export default ({mode}) => {
             emptyOutDir: true,
             manifest: true,
             target: 'es2018',
+        },
+        css: {
+            extract: true,
+            preprocessorOptions: {
+                scss: {}
+            },
+            postcss: {
+                plugins: [tailwindcss]
+            }
         },
         plugins: [
             laravel({
